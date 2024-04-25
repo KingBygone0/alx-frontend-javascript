404c76b4cd4e174f58035007cf2b5bc7082e1389
KingBygone0 <kingbygone@icloud.com>
2024-04-25 12:14:00 +0000

    hbtn course

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,12 @@
 export default class HolbertonCourse {
     constructor(name, length, students) {
-      if (typeof name !== 'string') throw new TypeError('name must be a string');
-      if (!Number.isInteger(length)) throw new TypeError('length must be a number');
-      if (!Array.isArray(students)) throw new TypeError('students type must be an Array');
+      if (typeof name !== 'string') {
+        throw new TypeError('Name must be a string');
+      } else if (typeof length !== 'number') {
+        throw new TypeError('Length must be a number');
+      } else if (!Array.isArray(students)) {
+        throw new TypeError('Students must be an array of strings');
+      }
   
       this._name = name;
       this._length = length;
@@ -13,26 +17,32 @@ export default class HolbertonCourse {
       return this._name;
     }
   
-    set name(val) {
-      if (typeof val !== 'string') throw new TypeError('name must be a string');
-      this._name = val;
-    }
-  
     get length() {
       return this._length;
-    }
-  
-    set length(val) {
-      if (!Number.isInteger(val)) throw new TypeError('length must be a number');
-      this._length = val;
     }
   
     get students() {
       return this._students;
     }
   
-    set students(val) {
-      if (!Array.isArray(val)) throw new TypeError('students type must be an Array');
-      this._students = val;
+    set name(name) {
+      if (typeof name !== 'string') {
+        throw new TypeError('Name must be a string');
+      }
+      this._name = name;
+    }
+  
+    set length(length) {
+      if (typeof length !== 'number') {
+        throw new TypeError('Length must be a number');
+      }
+      this._length = length;
+    }
+  
+    set students(students) {
+      if (!Array.isArray(students)) {
+        throw new TypeError('Students must be an array of strings');
+      }
+      this._students = students;
     }
   }

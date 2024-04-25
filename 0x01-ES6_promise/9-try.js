b9b4700a67bc38594ed3c8693cb51b2b81e12731
KingBygone0 <kingbygone@icloud.com>
2024-04-25 08:57:55 +0000

    try fix

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,11 @@
 export default function guardrail(mathFunction) {
-    const queue = [];
-    try {
-      queue.push(mathFunction());
-    } catch (error) {
-      queue.push('Error: cannot divide by 0');
-    } finally {
-      queue.push('Guardrail was processed');
-    }
-    return queue;
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (error) {
+    queue.push('Error: cannot divide by 0');
+  } finally {
+    queue.push('Guardrail was processed');
   }
-  
+  return queue;
+}

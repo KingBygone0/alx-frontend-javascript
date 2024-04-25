3be25370181864dc41ff2c0b29c81d7797265d12
KingBygone0 <kingbygone@icloud.com>
2024-04-25 08:42:59 +0000

    error fix

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -2,3 +2,4 @@ export default function loadBalancer(chinaDownload, USDownload) {
     return Promise.race([chinaDownload, USDownload])
       .then((value) => value);
   }
+  
--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -9,3 +9,4 @@ export default function guardrail(mathFunction) {
     }
     return queue;
   }
+  
